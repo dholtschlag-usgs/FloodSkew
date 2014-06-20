b123dfb837961737fd31a3716aa0c4f3d76eea84
dholtschlag-usgs <dholtschlag@usgs.gov>
2014-06-20 16:33:26 -0400

    Revised extract\Peak.. script processes all good data

--- src/Final Scripts/extractPeakStatsFromPrt.R
+++ src/Final Scripts/extractPeakStatsFromPrt.R
@@ -12,7 +12,7 @@ setwd("C:/Home/Projects/FloodSkew/Analysis/R/FloodSkew")
 gages = read.table("./data/miGagesIIa.txt", sep="\t",header=TRUE,
                    colClasses=c("character","character"))
 # 
-# Streamgages with regulated flow
+# Streamgages with regulated flow or superseded (more record at nearby site) or trend
 gagesReg = c("04034500","04035500","04036000","04044400","04058100","04058200",
              "04059000","04061500","04062000","04062500","04063000","04066800",
              "04067000","04108801","04114500","04128000","04129000","04133500",
